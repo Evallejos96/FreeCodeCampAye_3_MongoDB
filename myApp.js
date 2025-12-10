6726f5fffa2b65190f7144a9c95ab15181584556
Evallejos96 <96029483+Evallejos96@users.noreply.github.com>
2025-12-10 16:19:54 -0300

    Update myApp.js - Persona por ID

--- myApp.js
+++ myApp.js
@@ -58,8 +58,12 @@ const findOneByFood = (food, done) => {
 
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return done(err);
+    return done(null, data);
+  });
 };
+
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
